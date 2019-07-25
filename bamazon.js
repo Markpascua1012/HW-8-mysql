9bdd56d2c75d6f3ae43390c30a4cc152d0aeb5d2
Mark Pascua <markpascua1012@gmail.com>
2019-07-24 21:31:34 -0700

    finished first section

--- bamazon.js
+++ bamazon.js
@@ -18,24 +18,24 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected");
-    displayAll();
+    start();
 });
 
-function displayAll() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        for(var i = 0; i < res.length; i++){
-            console.log ("(" + res[i].id+ ")" + "(" + res[i].product_name + ")" + "(" + res[i].department_name + ")" + 
-            "(" + res[i].price + ")" +"(" + res[i].stock_quantity + ")" + "\n");
-        }
-        start();
-    });
-}
+// function displayAll() {
+//     connection.query("SELECT * FROM products", function (err, res) {
+//         for(var i = 0; i < res.length; i++){
+//             console.log ("(" + res[i].id+ ")" + "(" + res[i].product_name + ")" + "(" + res[i].department_name + ")" + 
+//             "(" + res[i].price + ")" +"(" + res[i].stock_quantity + ")" + "\n");
+//         }
+//         start();
+//     });
+// }
 
 function start() {
-    
+
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        
+
         inquirer
             .prompt([
                 {
@@ -44,29 +44,55 @@ function start() {
                     choices: function () {
                         var choiceArray = [];
                         for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].product_name);
+                            choiceArray.push({
+                                name: results[i].product_name,
+                                value: results[i]
+                            });
                         }
                         return choiceArray;
                     },
                     message: "what item would you like to buy."
                 },
-                {
-                    name: "itemQuantity",
-                    type: "input",
-                    message: "How many would you like to buy?"
-                }
-            ]).then(function (answer) {
-                if ((results.stock_quantity - answer.itemQuantity) > 0) {
-                    connection.query("UPDATE products SET stock_quantity='" + (results.stock_quantity - answer.itemQuantity)
-                        + "' WHERE product_name='" + "'", function (err, res2) {
-                            console.log("Product Bought!");
-                            displayAll();
-                        })
-                } else {
-                    console.log("We're out of that!")
-                    displayAll();
-                }
+
+            ])
+            .then(function (answer) {
+                var productChoice = answer.choice
+                console.log(productChoice)
+                itemQuan(productChoice)
+
             })
+        //    
+
+    })
+}
+
+function itemQuan(a) {
+    inquirer
+        .prompt([
+            {
+                name: "itemQuantity",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ]).then(function (answer) {
+            console.log(a)
+            console.log(a.stock_quantity)
+            var quantity = parseInt(answer.itemQuantity)
+            if ((a.stock_quantity - quantity) > 0) {
+                connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ? AND stock_quantity >= ?;", [answer.itemQuantity, a.id, answer.itemQuantity], function (err, res2) {
+                    console.log("Product Bought! your total is " + a.price * answer.itemQuantity);
+                });
+                start();
+            } else {
+                console.log("We're out of that!")
+                start();
+            }
+        })
+
+}
+
+
+
             // .then(function (answer) {
             //     var chosenItem;
             //     for (var i = 0; i < results.length; i++) {
@@ -93,12 +119,12 @@ function start() {
             //         );
             //     }
             //     else {
-                    
+
             //         console.log("your transaction was successful. you will be redirected to selection");
             //         displayAll();
             //     }
             // });
 
 
-    });
-}
+//     });
+// }
